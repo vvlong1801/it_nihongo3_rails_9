b1403decef2fd7612d0d69ce4f50623563fd6ce3
vvlong1801 <long.vv184146@sis.hust.edu.vn>
2021-12-14 03:27:26 +0000

    add detail book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[ show edit update destroy ]
   before_action :authenticate_user!
+
   # GET /books or /books.json
   def index
     @books = Book.all
@@ -64,6 +65,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :number_of_pages, :image)
+      params.require(:book).permit(:title, :number_of_pages, :image, :detail)
     end
 end
