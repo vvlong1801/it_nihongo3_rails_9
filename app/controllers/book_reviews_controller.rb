f62974f84f37786a4b32038591569e2bfb9c77dd
vvlong1801 <long.vv184146@sis.hust.edu.vn>
2021-12-14 05:49:46 +0000

    add review list screen

--- app/controllers/book_reviews_controller.rb
+++ app/controllers/book_reviews_controller.rb
@@ -1,12 +1,14 @@
 class BookReviewsController < ApplicationController
-  before_action :set_book_review, only: %i[ show edit update destroy ]
+  before_action :set_book_review, only: [:show, :edit, :update, :destroy]
 
-  # GET /book_reviews or /book_reviews.json
+  # GET /book_reviews
+  # GET /book_reviews.json
   def index
     @book_reviews = BookReview.all
   end
 
-  # GET /book_reviews/1 or /book_reviews/1.json
+  # GET /book_reviews/1
+  # GET /book_reviews/1.json
   def show
   end
 
@@ -19,39 +21,46 @@ class BookReviewsController < ApplicationController
   def edit
   end
 
-  # POST /book_reviews or /book_reviews.json
+  # POST /book_reviews
+  # POST /book_reviews.json
   def create
     @book_review = BookReview.new(book_review_params)
+    @book_review.user_id = current_user.id
 
     respond_to do |format|
       if @book_review.save
-        format.html { redirect_to @book_review, notice: "Book review was successfully created." }
+        url = "/books/" + @book_review.book_id.to_s
+        format.html { redirect_to url, notice: 'Book review was successfully created.' }
+        # format.html { redirect_to @book_review, notice: 'Book review was successfully created.' }
+        format.html { redirect_to @book_review, notice: 'Book review was successfully created.' }
         format.json { render :show, status: :created, location: @book_review }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new }
         format.json { render json: @book_review.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /book_reviews/1 or /book_reviews/1.json
+  # PATCH/PUT /book_reviews/1
+  # PATCH/PUT /book_reviews/1.json
   def update
     respond_to do |format|
       if @book_review.update(book_review_params)
-        format.html { redirect_to @book_review, notice: "Book review was successfully updated." }
+        format.html { redirect_to @book_review, notice: 'Book review was successfully updated.' }
         format.json { render :show, status: :ok, location: @book_review }
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        format.html { render :edit }
         format.json { render json: @book_review.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /book_reviews/1 or /book_reviews/1.json
+  # DELETE /book_reviews/1
+  # DELETE /book_reviews/1.json
   def destroy
     @book_review.destroy
     respond_to do |format|
-      format.html { redirect_to book_reviews_url, notice: "Book review was successfully destroyed." }
+      format.html { redirect_to book_reviews_url, notice: 'Book review was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
